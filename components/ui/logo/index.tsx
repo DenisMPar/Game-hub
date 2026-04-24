18a5ec5e8dc3277bfc41db480c05ba90177b0562
Denis Parada <denispar.dev@gmail.com>
2026-04-24 00:39:08 -0300

    feat: update logo to Gamepad2 and replace favicon with SVG

--- components/ui/logo/index.tsx
+++ components/ui/logo/index.tsx
@@ -1,9 +1,9 @@
-import { Swords } from "lucide-react";
+import { Gamepad2 } from "lucide-react";
 import classes from "./index.module.css";
 export function Logo() {
   return (
     <div className={classes.logo__root}>
-      <Swords />
+      <Gamepad2 />
     </div>
   );
 }
